6afc3eccc7f6f53c07f1ac2fa18fd6b9475123e5
Alain Richardt <blackrabbit17@protonmail.com>
2024-02-15 20:00:31 +1300

    Get model by ID

--- atomict/ml/model.py
+++ atomict/ml/model.py
@@ -3,3 +3,8 @@ from atomict.api import get
 
 def get_model(model_name: str):
     return get(f"api/ml-model/?short_desc={model_name}")
+
+
+def get_model_by_id(model_id: str):
+    return get(f"api/ml-model/{model_id}/?include_weights=true")
+
