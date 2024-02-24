c4b85453f955b326104c2af405cd754252de5722
Alain Richardt <blackrabbit17@protonmail.com>
2024-02-24 17:44:43 +1300

    Mol Fingerprinting

--- atomict/infra/streaming.py
+++ atomict/infra/streaming.py
@@ -6,9 +6,11 @@ TOPIC_CANTERA_SIMULATION = "procsim_cantera_simulate"
 TOPIC_CATALYSIS_EXPLORE = "catalysis_explore"
 TOPIC_CATALYSIS_SIMULATE = "catalysis_simulate"
 TOPIC_COUNTERFACTUAL_SIMULATE = "counterfactual_simulate"
+TOPIC_MOL_FINGERPRINT = "mol_fingerprint"
 
 GROUP_ID_CANTERA_SIMULATION = "procsim-cantera-simulation"
 GROUP_ID_CATALYSIS_EXPLORE = "catalysis-exploration"
+GROUP_ID_MOL_FINGERPRINT = "mol-fingerprint"
 
 
 def get_consumer():
--- atomict/ml/vectorizers/__init__.py
+++ atomict/ml/vectorizers/__init__.py
@@ -5,6 +5,11 @@ from .bitvect import avalon_fingerprints, morgan_fingerprints
 from .domainrelevant import domainrelevant_fingerprint
 
 
+# Better name, get_full_fingerprint will be deprecated
+def get_amd_fingerprint(smiles: str, remove_extraneous_hydrogens: bool = False):
+    return get_full_fingerprint(smiles, remove_extraneous_hydrogens)
+
+
 def get_full_fingerprint(smiles: str, remove_extraneous_hydrogens: bool = False):
 
     mol = Chem.MolFromSmiles(smiles)
