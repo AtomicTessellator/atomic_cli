9dee73335c1f0140f07ca83e632e3d96ede9da9b
Alain Richardt <blackrabbit17@protonmail.com>
2024-03-10 14:35:42 +1300

    Capture activesites

--- atomict/infra/streaming.py
+++ atomict/infra/streaming.py
@@ -1,3 +1,4 @@
+import logging
 import os
 
 from confluent_kafka import Consumer
@@ -13,13 +14,42 @@ TOPIC_KNOWLEDGE_EXTRACT = "knowledge_extract"
 def get_consumer():
     host = os.environ.get("AT_CONFLUENT_HOST")
     port = os.environ.get("AT_CONFLUENT_PORT")
-    app_id = os.environ.get("AT_CONFLUENT_APP_ID")
+    c_group = os.environ.get("AT_CONFLUENT_CONSUMER_GROUP")
+
+    session_timeout = os.environ.get("AT_CONFLUENT_SESSION_TIMEOUT_MS")
+    heartbeat_interval = os.environ.get("AT_CONFLUENT_HEARTBEAT_INTERVAL_MS")
+
+    try:
+        session_timeout = int(session_timeout)
+        logging.info(
+            f"AT_CONFLUENT_SESSION_TIMEOUT_MS configuration found: {session_timeout}"
+        )
+    except ValueError:
+        logging.warning(
+            f"Invalid session timeout: {session_timeout}, using default of 30000"
+        )
+        session_timeout = 30000
+
+    try:
+        heartbeat_interval = int(heartbeat_interval)
+        logging.info(
+            f"AT_CONFLUENT_HEARTBEAT_INTERVAL_MS configuration found: {heartbeat_interval}"
+        )
+    except ValueError:
+        logging.warning(
+            f"Invalid heartbeat interval: {heartbeat_interval}, using default of 10000"
+        )
+        heartbeat_interval = 10000
 
     c = Consumer(
         {
             "bootstrap.servers": f"{host}:{port}",
-            "group.id": app_id,
+            "group.id": c_group,
             "auto.offset.reset": "earliest",
+            "enable.auto.commit": False,
+            "session.timeout.ms": session_timeout,
+            "heartbeat.interval.ms": heartbeat_interval,
+            "max.poll.interval.ms": session_timeout,
         }
     )
 
