78c4564986c0a9c3582455e39d3d758bb67059b2
Alain Richardt <blackrabbit17@protonmail.com>
2024-04-26 17:56:48 +1200

    Min version python3.9

--- setup.py
+++ setup.py
@@ -36,6 +36,6 @@ setuptools.setup(
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
     ],
-    python_requires='>=3.11',
+    python_requires='>=3.9',
     install_requires=install_requires,
 )
