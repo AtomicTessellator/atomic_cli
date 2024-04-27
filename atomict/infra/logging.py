6f9e1db37d7fdcacec6fd69811852643e58c7460
Alain Richardt <blackrabbit17@protonmail.com>
2024-04-27 19:30:47 +1200

    Upgrade root logging config to support prefixes

--- atomict/infra/logging.py
+++ atomict/infra/logging.py
@@ -1,14 +1,15 @@
 from logging.config import dictConfig
 
 
-def config_loggers(*args, **kwargs):
+def config_loggers(prefix: str = '', *args, **kwargs):
     logging_config = dict(
         version=1,
-        formatters={"verbose": {"format": "%(levelname)s %(asctime)s %(message)s"}},
+        formatters={
+            "verbose": {"format": f"%(levelname)s %(asctime)s {prefix} %(message)s"}
+        },
         handlers={
             "console": {"class": "logging.StreamHandler", "formatter": "verbose"}
         },
         root={"handlers": ["console"], "level": "INFO"},
     )
-
     dictConfig(logging_config)
