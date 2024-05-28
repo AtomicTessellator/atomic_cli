99d313ce8e3e65d4f5117a3aceb2965203260b78
Alain Richardt <blackrabbit17@protonmail.com>
2024-05-28 22:26:33 +1200

    Phonon Upload Helper

---
+++ atomict/simulation/phonon.py
@@ -0,0 +1,14 @@
+from atomict.api import post
+
+
+def associate_user_upload_with_phonon_analysis(
+    user_upload_id: str, pa_id: str
+):
+    """
+    Associate a user upload with a Phonon Analysis
+    """
+    result = post(
+        "api/phonon-analysis-file/",
+        payload={"upload": user_upload_id, "analysis": pa_id},
+    )
+    return result
