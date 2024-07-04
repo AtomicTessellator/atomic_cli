e822cd93178bbe0f09a8c1e5c0dda5560839f9b9
Alain Richardt <blackrabbit17@protonmail.com>
2024-07-04 06:04:47 +0000

    Support project assignment on file upload

--- atomict/user/files.py
+++ atomict/user/files.py
@@ -1,11 +1,15 @@
 from atomict.api import post
 
 
-def upload_single_file(full_path: str, file_name: str):
+def upload_single_file(full_path: str, file_name: str, project_uuid: str = None):
+
+    payload = {
+        'users_name': file_name
+    }
+
+    if project_uuid:
+        payload['project_uuid'] = project_uuid
+
     with open(full_path, "rb") as f:
-        result = post(
-            "user/file_upload/",
-            files={file_name: f},
-            payload={'users_name': file_name}
-        )
+        result = post("user/file_upload/", files={file_name: f}, payload=payload)
         return result
