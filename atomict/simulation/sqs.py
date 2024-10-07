b6adc0bcedffe0b880956604907a5ed2f2706425
Alain Richardt <blackrabbit17@protonmail.com>
2024-10-07 02:17:58 +0000

    Bugfix: This get doesnt support params keyword

--- atomict/simulation/sqs.py
+++ atomict/simulation/sqs.py
@@ -6,7 +6,7 @@ def get_simulation(simulation_id: str, full: bool = False):
     Get a SQS simulation
     """
     if full:
-        result = get(f"api/sqs-exploration/{simulation_id}/", params={"full": full})
+        result = get(f"api/sqs-exploration/{simulation_id}/?full=true")
     else:
         result = get(f"api/sqs-exploration/{simulation_id}/")
 
