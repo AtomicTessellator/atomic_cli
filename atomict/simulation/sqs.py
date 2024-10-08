fd53dc4d6058c023d8e415d1d6e41615379f5c73
Alain Richardt <blackrabbit17@protonmail.com>
2024-10-08 02:39:17 +0000

    Add SQS file association

--- atomict/simulation/sqs.py
+++ atomict/simulation/sqs.py
@@ -1,4 +1,4 @@
-from atomict.api import get
+from atomict.api import get, post
 
 
 def get_simulation(simulation_id: str, full: bool = False):
@@ -11,3 +11,15 @@ def get_simulation(simulation_id: str, full: bool = False):
         result = get(f"api/sqs-exploration/{simulation_id}/")
 
     return result
+
+
+def associate_user_upload_with_sqs_simulation(
+    user_upload_id: str, sqs_simulation_id: str
+):
+    """
+    Associate a user upload with a SQS simulation
+    """
+    result = post(
+        "api/sqs-simulation-file/",
+        payload={"user_upload": user_upload_id, "simulation": sqs_simulation_id},
+    )
