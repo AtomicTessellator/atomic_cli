51df391081a124f472873a0671092a2c7813b707
Alain Richardt <blackrabbit17@protonmail.com>
2024-10-09 07:23:50 +0000

    KPoint Pipeline - Create k-point simulation

--- atomict/simulation/kpoint.py
+++ atomict/simulation/kpoint.py
@@ -1,4 +1,4 @@
-from atomict.api import get
+from atomict.api import get, post
 
 
 def get_kpoint_exploration(simulation_id: str):
@@ -14,4 +14,15 @@ def get_kpoint_simulation(simulation_id: str):
     Get KPoint simulation
     """
     result = get(f"api/kpoint-simulation/{simulation_id}/")
+    return result
+
+
+def create_kpoint_simulation(exploration_id: str, simulation_id: str, k_points: list[float]):
+    """
+    Create KPoint simulation
+    """
+    result = post(
+        f"api/kpoint-simulation/",
+        data={"exploration_id": exploration_id, "simulation_id": simulation_id, "k_points": k_points},
+    )
     return result
