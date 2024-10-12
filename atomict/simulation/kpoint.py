e0fc0fb71022e538508be73dedf360714c7c5616
Alain Richardt <blackrabbit17@protonmail.com>
2024-10-12 11:02:23 +0000

    Update k-point simulation endpoint

--- atomict/simulation/kpoint.py
+++ atomict/simulation/kpoint.py
@@ -1,4 +1,4 @@
-from atomict.api import get, post
+from atomict.api import get, post, patch
 
 
 def get_kpoint_exploration(simulation_id: str):
@@ -25,12 +25,26 @@ def get_kpoint_simulation(simulation_id: str):
     return result
 
 
-def create_kpoint_simulation(exploration_id: str, simulation_id: str, k_points: list[float]):
+def create_kpoint_simulation(
+    exploration_id: str, simulation_id: str, k_points: list[float]
+):
     """
     Create KPoint simulation
     """
     result = post(
         f"api/kpoint-simulation/",
-        payload={"exploration": exploration_id, "simulation": simulation_id, "k_points": k_points},
+        payload={
+            "exploration": exploration_id,
+            "simulation": simulation_id,
+            "k_points": k_points,
+        },
     )
+    return result
+
+
+def update_kpoint_simulation(simulation_id: str, fields: dict):
+    """
+    Update KPoint simulation
+    """
+    result = patch(f"api/kpoint-simulation/{simulation_id}/", payload=fields)
     return result
