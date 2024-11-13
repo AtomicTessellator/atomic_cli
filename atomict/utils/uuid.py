58c46812cd0f93cd6385aea086b72590b5e1b360
= <=>
2024-11-12 18:08:09 -0600

    remove: unused code

--- atomict/utils/uuid.py
+++
@@ -1,9 +0,0 @@
-import uuid
-
-
-def is_valid_uuid(uuid_string):
-    try:
-        uuid_obj = uuid.UUID(uuid_string, version=4)
-        return str(uuid_obj) == uuid_string
-    except ValueError:
-        return False
