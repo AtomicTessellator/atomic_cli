fc5af8db23060cfe193dff9d378bfd6b468ce3d5
Alain Richardt <blackrabbit17@protonmail.com>
2024-11-25 21:12:17 +0000

    Improvement: Sometimes FHIaims outputs hessian matrix data into the next step geometry which ASE cannot read, so fallback gracefully

--- atomict/utils/fhiaims/geometry.py
+++ atomict/utils/fhiaims/geometry.py
@@ -1,6 +1,8 @@
 from atomict.io.fhiaims import read_aims_output
 from ase.io import read
+from ase.io.formats import UnknownFileTypeError
 import os
+import logging
 
 
 def read_final_geometry(workspace_dir: str, simulation_id: str):
@@ -9,9 +11,13 @@ def read_final_geometry(workspace_dir: str, simulation_id: str):
     Args:
         workspace_dir (str): The workspace directory
     """
-    if os.path.exists(f"{workspace_dir}/starting_structure/geometry.in.next_step"):
-        return read(f"{workspace_dir}/starting_structure/geometry.in.next_step")
-    else:
-        return read_aims_output(
-            f"{workspace_dir}/starting_structure/{simulation_id}.out"
-        )[-1]
+    next_step_path = f"{workspace_dir}/starting_structure/geometry.in.next_step"
+    if os.path.exists(next_step_path):
+        try:
+            return read(next_step_path)
+        except UnknownFileTypeError:
+            logging.warning(f"Could not read {next_step_path}, falling back to output file")
+
+    return read_aims_output(
+        f"{workspace_dir}/starting_structure/{simulation_id}.out"
+    )[-1]
