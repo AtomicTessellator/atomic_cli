1aacee1615f452f6336bb456f68803b2a7d7a6ae
sha016 <sha0160@protonmail.com>
2026-04-15 23:14:17 +0000

    Revert "allow simulation id to be passed directly to clear_workspace"
    This reverts commit c28aa5b74ef91b3ef6981548f98820a5b8cf9f87.

--- atomict/user/workspace.py
+++ atomict/user/workspace.py
@@ -13,15 +13,10 @@ def display_name(user_upload):
     return user_upload["orig_name"]
 
 
-def clear_workspace(sim: dict | str, base_path: str = "./workspace"):
-    if isinstance(sim, str):
-        sim_id = sim
-    elif isinstance(sim, dict) and hasattr(sim, "id"):
-        sim_id = sim["id"]
-    else:
-        raise ValueError("Simulation parameter must be of type dict | str.")
+def clear_workspace(sim, base_path: str = "./workspace"):
 
-    target_dir = os.path.join(base_path, sim_id)
+    target_dir = os.path.join(base_path, sim["id"])
+
     if os.path.exists(target_dir):
         logging.warning(f"Removing existing workspace folder {target_dir}")
         shutil.rmtree(target_dir)
